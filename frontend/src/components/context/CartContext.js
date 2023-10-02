110848feca6e2feef83928da3bd555015bfe79d6
dipesh <thapaliyadipesh7@gmail.com>
2023-10-02 12:03:04 +0545

    changes added

--- frontend/src/components/context/CartContext.js
+++ frontend/src/components/context/CartContext.js
@@ -5,184 +5,55 @@ import Reducer from "./Reducer";
 const CartContext = createContext();
 
 function CartContextProvider({ children, session }) {
-   const initialValues = {
-      hideRightSidebar: true,
-      cart: [],
-      hideDropdown: true,
-      sidebarType: null,
-      registered: null,
-   };
+  const initialValues = {
+    hideRightSidebar: true,
+    cart: [],
+    hideDropdown: true,
+    sidebarType: null,
+    registered: null,
+  };
 
-   // // gets user and cart id from the session
-   // const cartId = session ? session.user.cart : null;
-   // const userId = session ? session.user.id : null;
+  //for managing global state
+  const [state, dispatch] = useReducer(Reducer, initialValues);
 
-   //for managing global state
-   const [state, dispatch] = useReducer(Reducer, initialValues);
+  //for managing cart state
+  const changeCartState = () => {
+    try {
+      dispatch({
+        type: "CHANGE_CART_STATE",
+        payload: { hideRightSidebar: !state.hideRightSidebar },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-   //for managing cart state
-   const changeSidebarState = (sidebarType) => {
-      try {
-         dispatch({
-            type: "CHANGE_CART_STATE",
-            payload: { hideRightSidebar: !state.hideRightSidebar, sidebarType },
-         });
-      } catch (error) {
-         console.log(error);
-      }
-   };
-
-   //for managing carts
-   const updateDbCart = async (cart) => {
-      try {
-         const response = await fetch(`/api/carts/${cartId}`, {
-            method: "PUT",
-            headers: {
-               "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ items: cart }),
-         });
-         return response;
-      } catch (error) {
-         console.log(error);
-      }
-   };
-
-   const addToCart = async (item, quantity = 1) => {
-      try {
-         let cart = state.cart;
-
-         if (!cart.filter((cartItem) => cartItem._id === item._id).length > 0) {
-            cart.push({ ...item, quantity });
-            dispatch({ type: "MANAGE_CART", payload: cart });
-            toast.success("Item added to cart !");
-
-            await updateDbCart(cart);
-         } else {
-            toast.warn("Item already in cart !");
-         }
-      } catch (error) {
-         console.log(error);
-      }
-   };
-
-   const removeFromCart = async (id) => {
+  //add item to cart
+  const addItemToCart = async (item, quantity = 1, leftQuantity) => {
+    try {
       let cart = state.cart;
-      cart = cart.filter((item) => item._id !== id);
-      try {
-         dispatch({ type: "MANAGE_CART", payload: cart });
-         await updateDbCart(cart);
-      } catch (error) {
-         console.log(error);
+
+      if (!cart.filter((cartItem) => cartItem._id === item._id).length > 0) {
+        cart.push({ ...item, quantity, leftQuantity });
+        // dispatch({ type: "MANAGE_CART", payload: cart });
+        console.log(state.cart);
       }
-   };
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-   const updateCart = async (id, quantity) => {
-      try {
-         let cart = state.cart;
-         cart = cart.map((item) => {
-            if (item._id === id) {
-               return { ...item, quantity: quantity };
-            } else {
-               return item;
-            }
-         });
-         dispatch({ type: "MANAGE_CART", payload: cart });
-         await updateDbCart(cart);
-      } catch (error) {
-         console.log(error);
-      }
-   };
-
-   // delete cart
-   const deleteCart = async () => {
-      try {
-         dispatch({ type: "MANAGE_CART", payload: [] });
-         await updateDbCart([]);
-      } catch (error) {
-         console.log(error);
-      }
-   };
-
-   const initCart = async () => {
-      try {
-         const response = await fetch(`/api/carts/${cartId}`);
-         const data = await response.json();
-         dispatch({ type: "MANAGE_CART", payload: data.items });
-      } catch (error) {
-         console.log(error);
-      }
-   };
-
-   //for managing dropdown
-   const changeDropDownState = () => {
-      try {
-         dispatch({
-            type: "CHANGE_DROPDOWN_STATE",
-            payload: !state.hideDropdown,
-         });
-      } catch (error) {
-         console.log(error);
-      }
-   };
-
-   //for checking out cart
-   const cartCheckout = async () => {
-      try {
-         state.cart.map((item) => {
-            (async () => {
-               const id = item._id;
-               const quantity = item.quantity;
-               const res = await fetch(
-                  `/api/products/${id}?updateType=checkout`,
-                  {
-                     method: "PUT",
-                     headers: {
-                        "content-type": "application/json",
-                     },
-                     body: JSON.stringify({ quantity }),
-                  }
-               );
-            })();
-         });
-
-         const res = await fetch(`/api/orders`, {
-            method: "POST",
-            headers: {
-               "content-type": "application/json",
-            },
-            body: JSON.stringify({ items: state.cart, user: userId }),
-         });
-         if (res.status == 201) {
-            deleteCart();
-            revalidatePath("/profile");
-         } else {
-            toast.error("Register you phone number first !");
-         }
-      } catch (e) {
-         console.log(e);
-      }
-   };
-
-   useEffect(() => {
-      if (cartId) initCart();
-   }, [cartId]);
-
-   return (
-      <CartContext.Provider
-         value={{
-            ...state,
-            changeSidebarState,
-            addToCart,
-            removeFromCart,
-            updateCart,
-            changeDropDownState,
-            cartCheckout,
-         }}
-      >
-         {children}
-      </CartContext.Provider>
-   );
+  return (
+    <CartContext.Provider
+      value={{
+        ...state,
+        changeCartState,
+        addItemToCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export default CartContext;
